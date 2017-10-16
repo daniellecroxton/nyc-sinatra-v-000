1bb012a705e0d61a84cb27a939f1ec246ef251ca
daniellecroxton <danikrist@gmail.com>
2017-10-16 11:57:55 -0600

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,18 @@
 class LandmarksController < ApplicationController
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
 
+  get '/landmarks/:id/edit' do
+    erb :'landmarks/edit'
+  end
+
+  get '/landmarks/:id' do
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
 end
